dbad5529f8f37d295359b1d7c28d261c9b870726
kushsaini10 <kushsaini.700@gmail.com>
2016-07-26 14:32:28 +0530

    first

--- app/src/main/java/com/example/kush/weathercast/MainActivity.java
+++ app/src/main/java/com/example/kush/weathercast/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.kush.weathercast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
 import java.util.ArrayList;
 
@@ -16,6 +18,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hourlyRecyclerView = (RecyclerView) findViewById(R.id.hourly_recycler_view);
+        data = new ArrayList<>();
+        data.add(new HourlyWF("30","20"));
+        data.add(new HourlyWF("30","20"));
+        data.add(new HourlyWF("30","20"));
+        data.add(new HourlyWF("30","20"));
+        data.add(new HourlyWF("30","20"));
+        data.add(new HourlyWF("30","20"));
+        data.add(new HourlyWF("30","20"));
+        data.add(new HourlyWF("30","20"));
+        data.add(new HourlyWF("30","20"));
+        data.add(new HourlyWF("30","20"));
+        data.add(new HourlyWF("30","20"));
+        data.add(new HourlyWF("30","20"));
+        data.add(new HourlyWF("30","20"));
+        adapter = new HourlyWVRAdapter(data,this);
+        hourlyRecyclerView.setAdapter(adapter);
+
+        LinearLayoutManager lm = new LinearLayoutManager(this);
+        lm.setOrientation(LinearLayoutManager.HORIZONTAL);
+        hourlyRecyclerView.setLayoutManager(lm);
+//
+//        ItemTouchHelper.SimpleCallback callback = new ItemTouchHelper.SimpleCallback(ItemTouchHelper.RIGHT)
 
     }
 }
